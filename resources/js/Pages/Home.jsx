30637e594ee20a65b5bda39d27b5f322bf703386
Mikelesnr <mwanza.n.m@gmail.com>
2024-10-27 16:49:33 +0200

    mobile responsive style fixes

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -33,11 +33,11 @@ const Home = () => {
                 </div>
             </section>
             <br />
-            <section id="about-me" class="section about-me-section">
-                <div class="container container-about">
+            <section id="about-me" className="section about-me-section">
+                <div className="container container-about">
                     <h2 className="body-h2">About Me</h2>
                     <div className="content about-me-content">
-                        <div class="info profile-info">
+                        <div className="info profile-info">
                             <h3 className="body-h3 ">Michael Mwanza</h3>
                             <p className="font-p">
                                 <strong>Date of Birth:</strong> April 18, 1990
@@ -54,7 +54,7 @@ const Home = () => {
                             </p>
                             <a
                                 href="documents/mike-resume.pdf"
-                                class="btn btn-download-cv"
+                                className="btn btn-download-cv"
                                 download
                             >
                                 Download CV
@@ -63,31 +63,110 @@ const Home = () => {
                     </div>
                 </div>
             </section>
-            <section id="Skills" class="section skills-section">
-                <div class=" container container-skills">
+            <section id="Skills" className="section skills-section">
+                <div className=" container container-skills">
                     <h2 className="body-h2">Skills</h2>
                     <div className="content skills-content">
                         <div className="info skills-info">
-                            <h3 className="body-h3">Skills List</h3>
-                            <p className="font-p">
-                                <strong>Date of Birth:</strong> April 18, 1990
-                            </p>
-                            <p className="font-p">
-                                <strong>Email:</strong>{" "}
-                                <a href="mailto:mwanza.n.m@gmail.com">
-                                    mwanza.n.m@gmail.com
-                                </a>
-                            </p>
-                            <p className="font-p">
-                                <strong>Address:</strong> 3 Alexander Road,
-                                Highlands, Harare
-                            </p>
+                            <h3 className="body-h3">My Skills</h3>
+                            <div className="skill-category">
+                                <h3>Languages</h3>
+                                <div className="skill">
+                                    <span>HTML: 100%</span>
+                                    <div
+                                        className="skill-bar"
+                                        style={{ width: "100%" }}
+                                    ></div>
+                                </div>
+                                <div className="skill">
+                                    <span>CSS: 90%</span>
+                                    <div
+                                        className="skill-bar"
+                                        style={{ width: "90%" }}
+                                    ></div>
+                                </div>
+                                <div className="skill">
+                                    <span>JavaScript: 95%</span>
+                                    <div
+                                        className="skill-bar"
+                                        style={{ width: "95%" }}
+                                    ></div>
+                                </div>
+                                <div className="skill">
+                                    <span>PHP: 85%</span>
+                                    <div
+                                        className="skill-bar"
+                                        style={{ width: "85%" }}
+                                    ></div>
+                                </div>
+                                <div className="skill">
+                                    <span>Python: 90%</span>
+                                    <div
+                                        className="skill-bar"
+                                        style={{ width: "90%" }}
+                                    ></div>
+                                </div>
+                                <div className="skill">
+                                    <span>MySQL: 90%</span>
+                                    <div
+                                        className="skill-bar"
+                                        style={{ width: "90%" }}
+                                    ></div>
+                                </div>
+                            </div>
+                            <div className="skill-category">
+                                <h3>Frameworks</h3>
+                                <div className="skill">
+                                    <span>React: 85%</span>
+                                    <div
+                                        className="skill-bar"
+                                        style={{ width: "85%" }}
+                                    ></div>
+                                </div>
+                                <div className="skill">
+                                    <span>Laravel: 85%</span>
+                                    <div
+                                        className="skill-bar"
+                                        style={{ width: "85%" }}
+                                    ></div>
+                                </div>
+                            </div>
+                            <div className="skill-category">
+                                <h3>DevOps</h3>
+                                <div className="skill">
+                                    <span>Linux: 95%</span>
+                                    <div
+                                        className="skill-bar"
+                                        style={{ width: "95%" }}
+                                    ></div>
+                                </div>
+                                <div className="skill">
+                                    <span>Git: 90%</span>
+                                    <div
+                                        className="skill-bar"
+                                        style={{ width: "90%" }}
+                                    ></div>
+                                </div>
+                                <div className="skill">
+                                    <span>Nginx: 85%</span>
+                                    <div
+                                        className="skill-bar"
+                                        style={{ width: "85%" }}
+                                    ></div>
+                                </div>
+                                <div className="skill">
+                                    <span>UFW: 100%</span>
+                                    <div
+                                        className="skill-bar"
+                                        style={{ width: "100%" }}
+                                    ></div>
+                                </div>
+                            </div>
                             <a
                                 href="path/to/your/cv.pdf"
-                                class="btn btn-projects"
-                                download
+                                className="btn btn-projects"
                             >
-                                projects
+                                Projects
                             </a>
                         </div>
                     </div>
