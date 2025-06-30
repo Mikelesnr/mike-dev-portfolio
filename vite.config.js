d3cf9c6f34116d14e46cf27f930012533c8fabf8
Michael Mwanza <mwanza.n.m@gmail.com>
2025-07-01 01:29:39 +0200

    vite config erro fix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import laravel from "laravel-vite-plugin";
+import * as dotenv from "dotenv";
 import { resolve } from "path";
 
 // Load .env manually from custom location
-path: resolve(__dirname, "resources/js/.env");
+dotenv.config({ path: resolve(__dirname, "resources/js/.env") });
 
 export default defineConfig({
     plugins: [
